5b3483a3aabbac4cabbe08f5f72ab80227b05bca
Nivedha Aravindmali <136926840+NvyGreen@users.noreply.github.com>
2026-06-24 17:23:04 +0000

    [LeetCode Sync] Runtime - 5 ms (84.66%), Memory - 23.8 MB (63.15%)

--- my-solutions/0124-binary-tree-maximum-path-sum/solution.py
+++ my-solutions/0124-binary-tree-maximum-path-sum/solution.py
@@ -8,14 +8,15 @@ class Solution:
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
         self.maxSum = float('-inf')
 
-        def helper(root):
-            if root is None:
+        def helperSum(root):
+            if not root:
                 return 0
             
-            left = max(helper(root.left), 0)
-            right = max(helper(root.right), 0)
+            left = max(helperSum(root.left), 0)
+            right = max(helperSum(root.right), 0)
             self.maxSum = max(self.maxSum, left + right + root.val)
-            return max(left, right) + root.val
+            return root.val + max(left, right)
         
-        helper(root)
+        helperSum(root)
         return self.maxSum
+        
