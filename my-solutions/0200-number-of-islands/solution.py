05fcb53a3dd59c36383a3ff2fc2275241868937b
Nivedha Aravindmali <136926840+NvyGreen@users.noreply.github.com>
2026-07-10 04:51:43 +0000

    [LeetCode Sync] Runtime - 231 ms (86.98%), Memory - 21.4 MB (91.98%)

---
+++ my-solutions/0200-number-of-islands/solution.py
@@ -0,0 +1,28 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        islands = 0
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == "1":
+                    islands += 1
+                    self.dfs(grid, i, j)
+        
+        return islands
+    
+
+    def dfs(self, grid: List[List[str]], row: int, col: int) -> None:
+        grid[row][col] = "0"
+        
+        if row + 1 < len(grid) and grid[row + 1][col] == "1":
+            self.dfs(grid, row + 1, col)
+        
+        if col + 1 < len(grid[0]) and grid[row][col + 1] == "1":
+            self.dfs(grid, row, col + 1)
+        
+        if row - 1 >= 0 and grid[row - 1][col] == "1":
+            self.dfs(grid, row - 1, col)
+        
+        if col - 1 >= 0 and grid[row][col - 1] == "1":
+            self.dfs(grid, row, col - 1)
+        
