6fcceef3f96b268ec1cbcbaa6291e02f7d48caf5
Nivedha Aravindmali <136926840+NvyGreen@users.noreply.github.com>
2026-07-11 21:25:37 +0000

    [LeetCode Sync] Runtime - 34 ms (50.20%), Memory - 21.8 MB (84.65%)

--- my-solutions/0560-subarray-sum-equals-k/solution.py
+++ my-solutions/0560-subarray-sum-equals-k/solution.py
@@ -1,13 +1,13 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        result = 0
-        prefix = 0
-        freq = {0: 1}
+        sum_count = {0: 1}
+        running_sum = 0
+        total_subarrays = 0
 
-        for num in nums:
-            prefix += num
-            result += freq.get(prefix - k, 0)
-            freq[prefix] = freq.get(prefix, 0) + 1
-
-        return result
+        for i in range(len(nums)):
+            running_sum += nums[i]
+            total_subarrays += sum_count.get(running_sum - k, 0)
+            sum_count[running_sum] = sum_count.get(running_sum, 0) + 1
+        
+        return total_subarrays
         
