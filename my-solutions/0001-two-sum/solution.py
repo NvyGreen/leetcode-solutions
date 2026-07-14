1c8305a20937b079c70efe0606ea067da016fe84
Nivedha Aravindmali <136926840+NvyGreen@users.noreply.github.com>
2026-07-14 17:30:47 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 20.5 MB (18.38%)

--- my-solutions/0001-two-sum/solution.py
+++ my-solutions/0001-two-sum/solution.py
@@ -1,12 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        if len(nums) == 2:
-            return [0, 1]
-        
-        lookup = {}
+        mp = {}
         for i in range(len(nums)):
-            if nums[i] in lookup:
-                return [lookup[nums[i]], i]
-            
-            lookup[target - nums[i]] = i
+            mp[nums[i]] = i
         
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in mp.keys() and mp[complement] != i:
+                return [i, mp[complement]]
+        
+        return -1
