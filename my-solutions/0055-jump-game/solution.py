a6873251b7d155b182a7856fd756a64ab6e9b25b
Nivedha Aravindmali <136926840+NvyGreen@users.noreply.github.com>
2026-07-24 05:01:43 +0000

    [LeetCode Sync] Runtime - 51 ms (12.51%), Memory - 20.6 MB (8.10%)

--- my-solutions/0055-jump-game/solution.py
+++ my-solutions/0055-jump-game/solution.py
@@ -1,32 +1,30 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        dp = [-1] * len(nums)
-        dp[0] = nums[0]
-        return self.checkJump(0, nums, dp)
-    
+        farthestReach = [-1] * len(nums)
+        index = 0
+        farthestReach[0] = nums[0]
+        destination = len(nums) - 1
 
-    def checkJump(self, index: int, nums: List[int], dp: List[int]):
-        if index == len(nums) - 1:
-            return True
-        
-        jumpLength = nums[index]
-        if jumpLength == 0:
-            return False
-        
-        maxJump = -1
-        maxIndex = -1
-        for i in range(index + 1, min(index + jumpLength + 1, len(nums))):
-            currJump = -1
-            if dp[i] != -1:
-                currJump = dp[i]
-            else:
-                currJump = nums[i] + i
-                dp[i] = currJump
+        while index < destination:
+            if farthestReach[index] == destination:
+                return True
             
-            if currJump > maxJump or (currJump == maxJump and i > maxIndex):
-                maxJump = currJump
-                maxIndex = i
-        
-        return self.checkJump(maxIndex, nums, dp)
+            if farthestReach[index] == index:
+                return False
             
-
+            maxJump = 0
+            maxIndex = 0
+            for i in range(index + 1, farthestReach[index] + 1):
+                if i > destination:
+                    break
+                if farthestReach[i] == -1:
+                    farthestReach[i] = i + nums[i]
+                if farthestReach[i] >= maxJump:
+                    maxJump = farthestReach[i]
+                    maxIndex = i
+            
+            if maxJump == 0:
+                return False
+            index = maxIndex
+        
+        return True
