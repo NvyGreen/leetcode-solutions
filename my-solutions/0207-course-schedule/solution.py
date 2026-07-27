5146cf97ef099af785c3f8f9e2d17f809e9a0564
Nivedha Aravindmali <136926840+NvyGreen@users.noreply.github.com>
2026-07-27 02:31:34 +0000

    [LeetCode Sync] Runtime - 3 ms (85.90%), Memory - 20.2 MB (93.03%)

--- my-solutions/0207-course-schedule/solution.py
+++ my-solutions/0207-course-schedule/solution.py
@@ -1,27 +1,24 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        edges = defaultdict(list)
         indegree = [0] * numCourses
-        adj_list = []
-        for i in range(numCourses):
-            adj_list.append([])
-        
         for course, prereq in prerequisites:
+            edges[prereq].append(course)
             indegree[course] += 1
-            adj_list[prereq].append(course)
         
         queue = deque([])
-        for i in range(len(indegree)):
+        order = []
+        for i in range(numCourses):
             if indegree[i] == 0:
                 queue.append(i)
         
-        top_list = []
         while len(queue) > 0:
-            node = queue.popleft()
-            for course in adj_list[node]:
-                indegree[course] -= 1
-                if indegree[course] == 0:
-                    queue.append(course)
-            
-            top_list.append(node)
+            course = queue.popleft()
+            if edges.get(course) is not None:
+                for newCourse in edges[course]:
+                    indegree[newCourse] -= 1
+                    if indegree[newCourse] == 0:
+                        queue.append(newCourse)
+            order.append(course)
         
-        return len(top_list) == numCourses
+        return len(order) == numCourses
