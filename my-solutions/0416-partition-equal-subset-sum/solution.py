572c9692dd717e403cef79984c8aef0d31cad094
Nivedha Aravindmali <136926840+NvyGreen@users.noreply.github.com>
2026-07-30 19:00:42 +0000

    [LeetCode Sync] Runtime - 1152 ms (37.86%), Memory - 19.3 MB (69.83%)

---
+++ my-solutions/0416-partition-equal-subset-sum/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        target = sum(nums)
+        if target % 2 == 1:
+            return False
+        
+        target //= 2
+        dp = [False] * (target + 1)
+        dp[0] = True
+
+        for num in nums:
+            for i in range(len(dp) - 1, -1, -1):
+                if i + num < len(dp) and dp[i]:
+                    dp[i + num] = True
+        
+        return dp[-1]
