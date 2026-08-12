7aeb7b9e4289a56516306b16da527b5f94a7e542
Nivedha Aravindmali <136926840+NvyGreen@users.noreply.github.com>
2026-08-12 18:36:39 +0000

    [LeetCode Sync] Runtime - 7 ms (21.93%), Memory - 19.4 MB (95.65%)

--- my-solutions/0684-redundant-connection/solution.py
+++ my-solutions/0684-redundant-connection/solution.py
@@ -1,28 +1,29 @@
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        uf = [0]
+        uf = []
         for i in range(len(edges)):
             uf.append(i + 1)
         
-        for node1, node2 in edges:
-            root1 = self.findRoot(node1, uf)
-            root2 = self.findRoot(node2, uf)
+        for edge in edges:
+            root1 = self.findRoot(edge[0], uf)
+            root2 = self.findRoot(edge[1], uf)
 
             if root1 == root2:
-                return [node1, node2]
+                return edge
             elif root1 < root2:
-                uf[root2] = root1
-            else:
-                uf[root1] = root2
+                uf[root2 - 1] = edge[0]
+            elif root1 > root2:
+                uf[root1 - 1] = edge[1]
         
-        # This shouldn't happen
         return [-1, -1]
     
 
     def findRoot(self, node: int, uf: List[int]) -> int:
         curr = node
-        nxt = uf[node]
+        nxt = uf[node - 1]
+
         while curr != nxt:
             curr = nxt
-            nxt = uf[nxt]
+            nxt = uf[nxt - 1]
+        
         return curr
