814da93cde1fde1aed97c04913b87ef76e166a91
Nivedha Aravindmali <136926840+NvyGreen@users.noreply.github.com>
2026-08-13 19:51:47 +0000

    [LeetCode Sync] Runtime - 18 ms (21.71%), Memory - 23.8 MB (30.41%)

--- my-solutions/0049-group-anagrams/solution.py
+++ my-solutions/0049-group-anagrams/solution.py
@@ -1,10 +1,9 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        mp = defaultdict(list)
+        groups = defaultdict(list)
         for word in strs:
-            freq = [0] * 26
-            for c in word:
-                freq[ord(c) - ord('a')] += 1
-            mp[tuple(freq)].append(word)
-        
-        return list(mp.values())
+            key = [0] * 26
+            for char in word:
+                key[ord(char) - ord('a')] += 1
+            groups[tuple(key)].append(word)
+        return list(groups.values())
