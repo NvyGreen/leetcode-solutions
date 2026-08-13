b8530f4cafa8fd15d1d839564ec9f0930d057dd6
Nivedha Aravindmali <136926840+NvyGreen@users.noreply.github.com>
2026-08-13 20:52:54 +0000

    [LeetCode Sync] Runtime - 3 ms (79.57%), Memory - 20.5 MB (37.81%)

--- my-solutions/0167-two-sum-ii---input-array-is-sorted/solution.py
+++ my-solutions/0167-two-sum-ii---input-array-is-sorted/solution.py
@@ -1,14 +1,14 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        start = 0
-        end = len(numbers) - 1
+        start, end = 0, len(numbers) - 1
 
         while start < end:
-            if numbers[start] + numbers[end] == target:
+            total = numbers[start] + numbers[end]
+            if total == target:
                 return [start + 1, end + 1]
-            elif numbers[start] + numbers[end] < target:
+            elif total < target:
                 start += 1
             else:
                 end -= 1
         
-        return [-1, -1]
+        return [0, 0]
