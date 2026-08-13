faa2867a4820c9428a3885df11d109e0d105adb5
Nivedha Aravindmali <136926840+NvyGreen@users.noreply.github.com>
2026-08-13 20:10:10 +0000

    [LeetCode Sync] Runtime - 3 ms (89.92%), Memory - 22.8 MB (60.50%)

--- my-solutions/0347-top-k-frequent-elements/solution.py
+++ my-solutions/0347-top-k-frequent-elements/solution.py
@@ -1,16 +1,16 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        freq = {}
+        freq = defaultdict(int)
         for num in nums:
-            freq[num] = freq.get(num, 0) + 1
+            freq[num] += 1
         
-        pq = []
-        for num, count in freq.items():
-            heapq.heappush(pq, (-count, num))
+        freq_queue = []
+        for num, freq_num in freq.items():
+            heapq.heappush(freq_queue, (-freq_num, num))
         
         result = []
         while len(result) < k:
-            _, num = heapq.heappop(pq)
+            _, num = heapq.heappop(freq_queue)
             result.append(num)
         
         return result
