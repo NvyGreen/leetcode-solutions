8a1b7747b9d64ea5f58740311437565025a6a064
Nivedha Aravindmali <136926840+NvyGreen@users.noreply.github.com>
2026-08-14 22:03:44 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.1 MB (90.93%)

--- my-solutions/0020-valid-parentheses/solution.py
+++ my-solutions/0020-valid-parentheses/solution.py
@@ -1,13 +1,12 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stk = []
-
         for c in s:
-            if c in ("(", "[", "{"):
+            if c in ('(', '[', '{'):
                 stk.append(c)
             elif len(stk) == 0:
                 return False
-            elif (c == ")" and stk[-1] == "(") or (c == "]" and stk[-1] == "[") or (c == "}" and stk[-1] == "{"):
+            elif (c == ')' and stk[-1] == '(') or (c == ']' and stk[-1] == '[') or (c == '}' and stk[-1] == '{'):
                 stk.pop()
             else:
                 return False
