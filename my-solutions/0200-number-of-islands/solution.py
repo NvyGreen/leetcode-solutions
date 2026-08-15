0e4dc6c9418b17e217b9c2d02f68cbddf2cb8c5e
Nivedha Aravindmali <136926840+NvyGreen@users.noreply.github.com>
2026-08-15 19:49:36 +0000

    [LeetCode Sync] Runtime - 234 ms (81.52%), Memory - 21.5 MB (91.98%)

--- my-solutions/0200-number-of-islands/solution.py
+++ my-solutions/0200-number-of-islands/solution.py
@@ -1,27 +1,25 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         islands = 0
-
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] == "1":
+                if grid[i][j] == '1':
                     islands += 1
-                    self.dfs(grid, i, j)
-
+                    self.islandsHelper(grid, i, j)
         return islands
     
 
-    def dfs(self, grid: List[List[str]], row: int, col: int):
-        grid[row][col] = "0"
+    def islandsHelper(self, grid: List[List[str]], row: int, col: int) -> None:
+        grid[row][col] = '0'
 
-        if col + 1 < len(grid[0]) and grid[row][col + 1] == "1":
-            self.dfs(grid, row, col + 1)
+        if col + 1 < len(grid[0]) and grid[row][col + 1] == '1':
+            self.islandsHelper(grid, row, col + 1)
         
-        if row + 1 < len(grid) and grid[row + 1][col] == "1":
-            self.dfs(grid, row + 1, col)
+        if row + 1 < len(grid) and grid[row + 1][col] == '1':
+            self.islandsHelper(grid, row + 1, col)
         
-        if col - 1 >= 0 and grid[row][col - 1] == "1":
-            self.dfs(grid, row, col - 1)
+        if col - 1 >= 0 and grid[row][col - 1] == '1':
+            self.islandsHelper(grid, row, col - 1)
         
-        if row - 1 >= 0 and grid[row - 1][col] == "1":
-            self.dfs(grid, row - 1, col)
+        if row - 1 >= 0 and grid[row - 1][col] == '1':
+            self.islandsHelper(grid, row - 1, col)
