4ba56c597548bf3bf8009c5b6774ce2dd6aef84f
Nivedha Aravindmali <136926840+NvyGreen@users.noreply.github.com>
2026-08-15 22:07:01 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.6 MB (13.27%)

--- my-solutions/1036-rotting-oranges/solution.py
+++ my-solutions/1036-rotting-oranges/solution.py
@@ -1,42 +1,39 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        m = len(grid)
-        n = len(grid[0])
-
         queue = deque([])
-        for i in range(m):
-            for j in range(n):
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
                 if grid[i][j] == 2:
                     queue.append([i, j])
         
-        minutes = 0
+        time = 0
         while len(queue) > 0:
             roundLength = len(queue)
-
             for _ in range(roundLength):
                 row, col = queue.popleft()
-                if col + 1 < n and grid[row][col + 1] == 1:
-                    queue.append([row, col + 1])
+
+                if col + 1 < len(grid[0]) and grid[row][col + 1] == 1:
                     grid[row][col + 1] = 2
+                    queue.append([row, col + 1])
                 
-                if row + 1 < m and grid[row + 1][col] == 1:
-                    queue.append([row + 1, col])
+                if row + 1 < len(grid) and grid[row + 1][col] == 1:
                     grid[row + 1][col] = 2
+                    queue.append([row + 1, col])
                 
                 if col - 1 >= 0 and grid[row][col - 1] == 1:
-                    queue.append([row, col - 1])
                     grid[row][col - 1] = 2
+                    queue.append([row, col - 1])
                 
                 if row - 1 >= 0 and grid[row - 1][col] == 1:
-                    queue.append([row - 1, col])
                     grid[row - 1][col] = 2
+                    queue.append([row - 1, col])
             
             if len(queue) > 0:
-                minutes += 1
+                time += 1
         
-        for i in range(m):
-            for j in range(n):
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
                 if grid[i][j] == 1:
                     return -1
         
-        return minutes
+        return time
