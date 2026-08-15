48fe9914ff2fa961ea362f8aeac8c68a561cd148
Nivedha Aravindmali <136926840+NvyGreen@users.noreply.github.com>
2026-08-15 04:05:35 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.5 MB (5.53%)

--- my-solutions/0078-subsets/solution.py
+++ my-solutions/0078-subsets/solution.py
@@ -1,17 +1,19 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        allSubsets = [[]]
+        result = [[]]
         for i in range(len(nums)):
-            allSubsets.append([nums[i]])
-            allSubsets += self.subsetHelper(nums, [nums[i]], i)
-        return allSubsets
+            running = [nums[i]]
+            result.append(running)
+            result += self.subsetHelper(nums, running, i + 1)
+        return result
     
 
-    def subsetHelper(self, nums: List[int], running: List[int], index: int):
+    def subsetHelper(self, nums: List[int], running: List[int], startIndex: int) -> List[List[int]]:
         result = []
-        for i in range(index + 1, len(nums)):
-            newRunning = running + [nums[i]]
-            result.append(newRunning)
-            result += self.subsetHelper(nums, newRunning, i)
+        if startIndex >= len(nums):
+            return result
         
+        for i in range(startIndex, len(nums)):
+            result.append(running + [nums[i]])
+            result += self.subsetHelper(nums, running + [nums[i]], i + 1)
         return result
