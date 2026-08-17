bac5b2ceda5fc9e6cfabb0fc9302bcca8b83a13a
Nivedha Aravindmali <136926840+NvyGreen@users.noreply.github.com>
2026-08-17 05:49:15 +0000

    [LeetCode Sync] Runtime - 58 ms (7.12%), Memory - 31.6 MB (6.61%)

--- my-solutions/0053-maximum-subarray/solution.py
+++ my-solutions/0053-maximum-subarray/solution.py
@@ -1,12 +1,7 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return nums[0]
-        
         dp = [0] * len(nums)
         dp[-1] = nums[-1]
-
         for i in range(len(nums) - 2, -1, -1):
             dp[i] = max(nums[i], nums[i] + dp[i + 1])
-        
         return max(dp)
