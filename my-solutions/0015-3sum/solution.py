da8526331cbbbccd4c30fddd89b90435df33dfe2
Nivedha Aravindmali <136926840+NvyGreen@users.noreply.github.com>
2026-08-17 05:35:34 +0000

    [LeetCode Sync] Runtime - 524 ms (82.04%), Memory - 22.3 MB (32.32%)

--- my-solutions/0015-3sum/solution.py
+++ my-solutions/0015-3sum/solution.py
@@ -2,28 +2,34 @@ class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
         result = []
         nums.sort()
+
         for i in range(len(nums)):
             if nums[i] > 0:
                 break
-            if i == 0 or nums[i - 1] != nums[i]:
-                result += self.twoSumHelper(nums, nums[i], i)
+            if i == 0 or nums[i] != nums[i - 1]:
+                result += self.twoSumHelper(nums, i)
+        
         return result
     
 
-    def twoSumHelper(self, nums: list[int], pivotNum: int, pivot: int) -> list[list[int]]:
-        result = []
+    def twoSumHelper(self, nums: list[int], pivot: int) -> list[list[int]]:
         start, end = pivot + 1, len(nums) - 1
+        result = []
+
         while start < end:
-            total = pivotNum + nums[start] + nums[end]
-            if total < 0:
+            total = nums[start] + nums[end]
+            if total < -nums[pivot]:
                 start += 1
-            elif total > 0:
+            elif total > -nums[pivot]:
                 end -= 1
             else:
-                result.append([pivotNum, nums[start], nums[end]])
+                result.append([nums[pivot], nums[start], nums[end]])
                 start += 1
-                end -= 1
-                while start < end and nums[start] == nums[start - 1]:
+                while start < end and nums[start - 1] == nums[start]:
                     start += 1
-
+                
+                end -= 1
+                while start < end and nums[end + 1] == nums[end]:
+                    end -= 1
+        
         return result
