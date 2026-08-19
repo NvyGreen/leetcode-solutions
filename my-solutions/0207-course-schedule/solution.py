1b2e200366ac54128a1167fd6fdab66d9e8dd4ba
Nivedha Aravindmali <136926840+NvyGreen@users.noreply.github.com>
2026-08-19 02:34:58 +0000

    [LeetCode Sync] Runtime - 4 ms (64.12%), Memory - 20.3 MB (93.03%)

--- my-solutions/0207-course-schedule/solution.py
+++ my-solutions/0207-course-schedule/solution.py
@@ -1,24 +1,23 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        edges = defaultdict(list)
+        adjList = defaultdict(list)
         indegree = [0] * numCourses
         for course, prereq in prerequisites:
-            edges[prereq].append(course)
+            adjList[prereq].append(course)
             indegree[course] += 1
         
         queue = deque([])
-        order = []
         for i in range(numCourses):
             if indegree[i] == 0:
                 queue.append(i)
         
+        coursesTaken = 0
         while len(queue) > 0:
-            course = queue.popleft()
-            if edges.get(course) is not None:
-                for newCourse in edges[course]:
-                    indegree[newCourse] -= 1
-                    if indegree[newCourse] == 0:
-                        queue.append(newCourse)
-            order.append(course)
+            prereq = queue.popleft()
+            for course in adjList[prereq]:
+                indegree[course] -= 1
+                if indegree[course] == 0:
+                    queue.append(course)
+            coursesTaken += 1
         
-        return len(order) == numCourses
+        return coursesTaken == numCourses
