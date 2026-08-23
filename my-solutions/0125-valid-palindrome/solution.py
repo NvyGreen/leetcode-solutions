cdcf202302c64deeacd82b030173508bc251f2e1
Nivedha Aravindmali <136926840+NvyGreen@users.noreply.github.com>
2026-08-23 04:45:35 +0000

    [LeetCode Sync] Runtime - 9 ms (40.99%), Memory - 19.6 MB (53.81%)

--- my-solutions/0125-valid-palindrome/solution.py
+++ my-solutions/0125-valid-palindrome/solution.py
@@ -1,18 +1,16 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         start, end = 0, len(s) - 1
-
         while start < end:
-            while start < len(s) and not s[start].isalnum():
+            while start < end and not s[start].isalnum():
                 start += 1
-            while end >= 0 and not s[end].isalnum():
+            
+            while start < end and not s[end].isalnum():
                 end -= 1
-            if start >= end:
-                return True
             
             if s[start].lower() != s[end].lower():
                 return False
+            
             start += 1
             end -= 1
-
         return True
