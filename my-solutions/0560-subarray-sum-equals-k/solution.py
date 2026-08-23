35aecdcb7e126100153a0368d43cad7f90cd08a3
Nivedha Aravindmali <136926840+NvyGreen@users.noreply.github.com>
2026-08-23 17:37:31 +0000

    [LeetCode Sync] Runtime - 39 ms (27.79%), Memory - 22.3 MB (14.92%)

--- my-solutions/0560-subarray-sum-equals-k/solution.py
+++ my-solutions/0560-subarray-sum-equals-k/solution.py
@@ -1,12 +1,13 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
+        running, result = 0, 0
         complement = defaultdict(int)
         complement[0] = 1
 
-        total, running = 0, 0
-        for num in nums:
-            running += num
+        for i in range(len(nums)):
+            running += nums[i]
             if running - k in complement:
-                total += complement[running - k]
+                result += complement[running - k]
             complement[running] += 1
-        return total
+        
+        return result
