50eb8d4436838512272b1db8d33633c620eda39c
Nivedha Aravindmali <136926840+NvyGreen@users.noreply.github.com>
2026-08-23 17:10:31 +0000

    [LeetCode Sync] Runtime - 729 ms (25.15%), Memory - 22.2 MB (54.55%)

--- my-solutions/0015-3sum/solution.py
+++ my-solutions/0015-3sum/solution.py
@@ -1,12 +1,13 @@
 class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
-        result = []
         nums.sort()
+        result = []
 
         for i in range(len(nums)):
             if nums[i] > 0:
                 break
-            if i == 0 or nums[i] != nums[i - 1]:
+            
+            if i == 0 or nums[i - 1] != nums[i]:
                 result += self.twoSumHelper(nums, i)
         
         return result
@@ -15,19 +16,26 @@ class Solution:
     def twoSumHelper(self, nums: list[int], pivot: int) -> list[list[int]]:
         start, end = pivot + 1, len(nums) - 1
         result = []
-
+        
         while start < end:
-            total = nums[start] + nums[end]
-            if total < -nums[pivot]:
-                start += 1
-            elif total > -nums[pivot]:
-                end -= 1
-            else:
+            total = nums[pivot] + nums[start] + nums[end]
+            if total == 0:
                 result.append([nums[pivot], nums[start], nums[end]])
                 start += 1
+                end -= 1
+
                 while start < end and nums[start - 1] == nums[start]:
                     start += 1
                 
+                while start < end and nums[end + 1] == nums[end]:
+                    end -= 1
+            
+            elif total < 0:
+                start += 1
+                while start < end and nums[start - 1] == nums[start]:
+                    start += 1
+            
+            else:
                 end -= 1
                 while start < end and nums[end + 1] == nums[end]:
                     end -= 1
