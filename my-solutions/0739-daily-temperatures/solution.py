a5ea3bb903a8131844639e4cdbe811de136dae1b
Nivedha Aravindmali <136926840+NvyGreen@users.noreply.github.com>
2026-08-24 16:53:59 +0000

    [LeetCode Sync] Runtime - 103 ms (42.15%), Memory - 28.2 MB (69.24%)

--- my-solutions/0739-daily-temperatures/solution.py
+++ my-solutions/0739-daily-temperatures/solution.py
@@ -1,12 +1,12 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stk = []
         answer = [0] * len(temperatures)
-        dec_stk = []
 
         for i in range(len(temperatures)):
-            while len(dec_stk) > 0 and temperatures[dec_stk[-1]] < temperatures[i]:
-                old_index = dec_stk.pop()
-                answer[old_index] = i - old_index
-            dec_stk.append(i)
-
+            while len(stk) > 0 and temperatures[stk[-1]] < temperatures[i]:
+                answer[stk[-1]] = i - stk[-1]
+                stk.pop()
+            stk.append(i)
+        
         return answer
