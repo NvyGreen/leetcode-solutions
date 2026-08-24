1b6cd22280da30a2323e331ace2b0c80e8733f42
Nivedha Aravindmali <136926840+NvyGreen@users.noreply.github.com>
2026-08-24 18:07:55 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.3 MB (73.66%)

--- my-solutions/0054-spiral-matrix/solution.py
+++ my-solutions/0054-spiral-matrix/solution.py
@@ -1,33 +1,33 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        spiral, visited = [], []
         m, n = len(matrix), len(matrix[0])
+        visited = []
         for i in range(m):
-            row = []
-            for j in range(n):
-                row.append(False)
+            row = [False] * n
             visited.append(row)
         
+        spiral = []
+        rowDelta, colDelta = 0, 1
         row, col = 0, 0
-        rowDir, colDir = 0, 1
         while len(spiral) < m * n:
             spiral.append(matrix[row][col])
             visited[row][col] = True
 
-            if colDir == 1 and (col + colDir >= n or visited[row][col + colDir]):
-                colDir = 0
-                rowDir = 1
-            elif rowDir == 1 and (row + rowDir >= m or visited[row + rowDir][col]):
-                colDir = -1
-                rowDir = 0
-            elif colDir == -1 and (col + colDir < 0 or visited[row][col + colDir]):
-                colDir = 0
-                rowDir = -1
-            elif rowDir == -1 and (row + rowDir < 0 or visited[row + rowDir][col]):
-                colDir = 1
-                rowDir = 0
+            if row + rowDelta < 0 or row + rowDelta >= m or col + colDelta < 0 or col + colDelta >= n or visited[row + rowDelta][col + colDelta]:
+                if rowDelta == 0 and colDelta == 1:
+                    rowDelta = 1
+                    colDelta = 0
+                elif rowDelta == 1 and colDelta == 0:
+                    rowDelta = 0
+                    colDelta = -1
+                elif rowDelta == 0 and colDelta == -1:
+                    rowDelta = -1
+                    colDelta = 0
+                elif rowDelta == -1 and colDelta == 0:
+                    rowDelta = 0
+                    colDelta = 1
             
-            row += rowDir
-            col += colDir
+            row += rowDelta
+            col += colDelta
         
         return spiral
