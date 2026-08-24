ef2529bcdd43c1d83d56a348e0251d5d7d77471c
Nivedha Aravindmali <136926840+NvyGreen@users.noreply.github.com>
2026-08-24 18:16:58 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.4 MB (66.73%)

--- my-solutions/0733-flood-fill/solution.py
+++ my-solutions/0733-flood-fill/solution.py
@@ -8,17 +8,17 @@ class Solution:
         return image
     
 
-    def dfs(self, image: List[List[int]], row: int, col: int, color: int, ogColor: int):
-        image[row][col] = color
+    def dfs(self, image: List[List[int]], sr: int, sc: int, color: int, ogColor: int):
+        image[sr][sc] = color
+
+        if sc + 1 < len(image[0]) and image[sr][sc + 1] == ogColor:
+            self.dfs(image, sr, sc + 1, color, ogColor)
         
-        if col + 1 < len(image[0]) and image[row][col + 1] == ogColor:
-            self.dfs(image, row, col + 1, color, ogColor)
+        if sr + 1 < len(image) and image[sr + 1][sc] == ogColor:
+            self.dfs(image, sr + 1, sc, color, ogColor)
         
-        if row + 1 < len(image) and image[row + 1][col] == ogColor:
-            self.dfs(image, row + 1, col, color, ogColor)
+        if sc - 1 >= 0 and image[sr][sc - 1] == ogColor:
+            self.dfs(image, sr, sc - 1, color, ogColor)
         
-        if col - 1 >= 0 and image[row][col - 1] == ogColor:
-            self.dfs(image, row, col - 1, color, ogColor)
-        
-        if row - 1 >= 0 and image[row - 1][col] == ogColor:
-            self.dfs(image, row - 1, col, color, ogColor)
+        if sr - 1 >= 0 and image[sr - 1][sc] == ogColor:
+            self.dfs(image, sr - 1, sc, color, ogColor)
