acec0a0434112fde4f905da082becb32db9b6b48
Nivedha Aravindmali <136926840+NvyGreen@users.noreply.github.com>
2026-08-27 00:02:33 +0000

    [LeetCode Sync] Runtime - 55 ms (59.29%), Memory - 19.6 MB (33.32%)

---
+++ my-solutions/0696-count-binary-substrings/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countBinarySubstrings(self, s: str) -> int:
+        oldGroup, newGroup, count = 0, 0, 0
+
+        for i in range(len(s)):
+            if i > 0 and s[i - 1] != s[i]:
+                count += min(oldGroup, newGroup)
+                oldGroup, newGroup = newGroup, 0
+            newGroup += 1
+        
+        return count + min(oldGroup, newGroup)
