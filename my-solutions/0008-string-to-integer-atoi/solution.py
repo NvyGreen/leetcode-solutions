8b68715fb2a6a9da1b4d1ca3b9749017522bd9e3
Nivedha Aravindmali <136926840+NvyGreen@users.noreply.github.com>
2026-08-27 22:56:34 +0000

    [LeetCode Sync] Runtime - 3 ms (34.15%), Memory - 19.2 MB (80.93%)

--- my-solutions/0008-string-to-integer-atoi/solution.py
+++ my-solutions/0008-string-to-integer-atoi/solution.py
@@ -1,30 +1,25 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
         num = 0
-        neg = None
+        digits = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
         started = False
+        flip = False
 
         for c in s:
             if c == ' ' and not started:
                 continue
             elif c == '+' and not started:
-                neg = False
                 started = True
             elif c == '-' and not started:
-                neg = True
+                flip = True
                 started = True
-            elif ord(c) - ord('0') >= 0 and ord(c) - ord('0') <= 9:
-                num = 10 * num + (ord(c) - ord('0'))
+            elif c in digits:
+                num = num * 10 + int(c)
                 started = True
             else:
                 break
         
-        if neg:
-            num = -num
-
-        if num < -2 ** 31:
-            return -2 ** 31
-        if num > 2 ** 31 - 1:
-            return 2 ** 31 - 1
-
-        return num
+        if flip:
+            return max(-2 ** 31, -num)
+        else:
+            return min(2 ** 31 - 1, num)
