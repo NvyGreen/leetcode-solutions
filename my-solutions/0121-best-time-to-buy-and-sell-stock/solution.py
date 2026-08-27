e2cb24bacb299b841fbae2a51d8beac18ac3886c
Nivedha Aravindmali <136926840+NvyGreen@users.noreply.github.com>
2026-08-27 00:04:49 +0000

    [LeetCode Sync] Runtime - 59 ms (33.63%), Memory - 28.6 MB (76.24%)

--- my-solutions/0121-best-time-to-buy-and-sell-stock/solution.py
+++ my-solutions/0121-best-time-to-buy-and-sell-stock/solution.py
@@ -1,8 +1,10 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        maxSell = 0
-        maxPrice = prices[-1]
-        for i in range(len(prices) - 2, -1, -1):
-            maxSell = max(maxPrice - prices[i], maxSell)
-            maxPrice = max(prices[i], maxPrice)
-        return maxSell
+        minPrice = prices[0]
+        maxProf = 0
+
+        for i in range(1, len(prices)):
+            maxProf = max(maxProf, prices[i] - minPrice)
+            minPrice = min(minPrice, prices[i])
+        
+        return maxProf
