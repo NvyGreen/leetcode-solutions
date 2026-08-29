1a6f1547d9b22c3fb8a3377b33006f536d0f3c90
Nivedha Aravindmali <136926840+NvyGreen@users.noreply.github.com>
2026-08-29 01:14:25 +0000

    [LeetCode Sync] Runtime - 4 ms (75.37%), Memory - 19.6 MB (48.18%)

--- my-solutions/1829-maximum-units-on-a-truck/solution.py
+++ my-solutions/1829-maximum-units-on-a-truck/solution.py
@@ -1,17 +1,16 @@
 class Solution:
     def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
         boxTypes.sort(key=lambda box: box[1], reverse=True)
-        load, units, index = 0, 0, 0
+        totalCount, totalUnits = 0, 0
 
-        while index < len(boxTypes) and load < truckSize:
-            count, weight = boxTypes[index]
-            print(load + count)
-            if load + count > truckSize:
-                count -= load + count - truckSize
-            print(count, weight)
+        for i in range(len(boxTypes)):
+            count, units = boxTypes[i]
+            if totalCount + count > truckSize:
+                count -= count + totalCount - truckSize
             
-            load += count
-            units += count * weight
-            index += 1
+            totalCount += count
+            totalUnits += count * units
+            if totalCount >= truckSize:
+                break
         
-        return units
+        return totalUnits
