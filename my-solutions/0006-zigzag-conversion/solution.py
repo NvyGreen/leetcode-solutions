41b412cf5d355d256106e14d4a647f67f53ae9cc
Nivedha Aravindmali <136926840+NvyGreen@users.noreply.github.com>
2026-08-29 01:24:33 +0000

    [LeetCode Sync] Runtime - 19 ms (13.18%), Memory - 19.3 MB (80.23%)

--- my-solutions/0006-zigzag-conversion/solution.py
+++ my-solutions/0006-zigzag-conversion/solution.py
@@ -1,23 +1,19 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        if numRows == 1:
+        if len(s) == 1 or numRows == 1:
             return s
-        zig = []
-        dist = [2 * (numRows - 1), 0]
-        start = 0
-
-        while start < numRows:
-            pos = start
-            move = 0
-
-            while pos < len(s):
-                if dist[move] != 0:
-                    zig.append(s[pos])
-                pos += dist[move]
-                move = (move + 1) % 2
-            
-            start += 1
-            dist[0] -= 2
-            dist[1] += 2
         
-        return ''.join(zig)
+        result = []
+        direction = [2 * (numRows - 1), 0]
+
+        for start in range(numRows):
+            i, move = start, 0
+            while i < len(s):
+                if direction[move] != 0:
+                    result.append(s[i])
+                i += direction[move]
+                move = (move + 1) % 2
+            direction[0] -= 2
+            direction[1] += 2
+        
+        return ''.join(result)
