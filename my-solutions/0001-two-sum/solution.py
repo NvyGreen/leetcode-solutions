a2261649779c490df366ab18559f1ae237c61f8d
Nivedha Aravindmali <136926840+NvyGreen@users.noreply.github.com>
2026-08-29 00:51:52 +0000

    [LeetCode Sync] Runtime - 3 ms (53.62%), Memory - 20.5 MB (18.38%)

--- my-solutions/0001-two-sum/solution.py
+++ my-solutions/0001-two-sum/solution.py
@@ -2,9 +2,7 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         complement = {}
         for i in range(len(nums)):
-            num = nums[i]
-            if target - num in complement:
-                return [complement[target - num], i]
-            complement[num] = i
-        
+            if target - nums[i] in complement:
+                return [complement[target - nums[i]], i]
+            complement[nums[i]] = i
         return [-1, -1]
