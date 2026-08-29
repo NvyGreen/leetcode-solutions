0d7175865c55a74c3606dad53650a95dab108502
Nivedha Aravindmali <136926840+NvyGreen@users.noreply.github.com>
2026-08-29 01:38:13 +0000

    [LeetCode Sync] Runtime - 72 ms (46.80%), Memory - 33.4 MB (77.58%)

--- my-solutions/3151-minimum-processing-time/solution.py
+++ my-solutions/3151-minimum-processing-time/solution.py
@@ -5,7 +5,7 @@ class Solution:
         maxTime = 0
 
         for i in range(len(processorTime)):
-            start = i * 4
-            maxTime = max(maxTime, processorTime[i] + max(tasks[start:start+4]))
+            time = processorTime[i] + max(tasks[i * 4: i * 4 + 4])
+            maxTime = max(maxTime, time)
         
         return maxTime
