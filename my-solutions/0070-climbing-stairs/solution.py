b3de9419124ce71b3f0cd8ac4ed750472011a91e
Nivedha Aravindmali <136926840+NvyGreen@users.noreply.github.com>
2026-08-30 18:44:57 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.1 MB (86.86%)

--- my-solutions/0070-climbing-stairs/solution.py
+++ my-solutions/0070-climbing-stairs/solution.py
@@ -1,6 +1,8 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        dp = [1] * (n + 1)
+        dp = [0] * (n + 1)
+        dp[-1], dp[-2] = 1, 1
+
         for i in range(len(dp) - 3, -1, -1):
             dp[i] = dp[i + 1] + dp[i + 2]
 
