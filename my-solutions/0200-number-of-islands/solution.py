f69470a215ae3c0ee724bb998066e3d1b1465d96
Nivedha Aravindmali <136926840+NvyGreen@users.noreply.github.com>
2026-08-30 18:41:33 +0000

    [LeetCode Sync] Runtime - 223 ms (94.97%), Memory - 21.5 MB (72.88%)

--- my-solutions/0200-number-of-islands/solution.py
+++ my-solutions/0200-number-of-islands/solution.py
@@ -3,23 +3,23 @@ class Solution:
         islands = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] == "1":
+                if grid[i][j] == '1':
                     islands += 1
-                    self.islandsHelper(grid, i, j)
+                    self.helper(grid, i, j)
         return islands
     
 
-    def islandsHelper(self, grid: List[List[str]], row: int, col: int) -> None:
-        grid[row][col] = "0"
+    def helper(self, grid: List[List[str]], row: int, col: int) -> None:
+        grid[row][col] = '0'
+
+        if row + 1 < len(grid) and grid[row + 1][col] == '1':
+            self.helper(grid, row + 1, col)
         
-        if col + 1 < len(grid[0]) and grid[row][col + 1] == "1":
-            self.islandsHelper(grid, row, col + 1)
+        if col + 1 < len(grid[0]) and grid[row][col + 1] == '1':
+            self.helper(grid, row, col + 1)
         
-        if row + 1 < len(grid) and grid[row + 1][col] == "1":
-            self.islandsHelper(grid, row + 1, col)
+        if row - 1 >= 0 and grid[row - 1][col] == '1':
+            self.helper(grid, row - 1, col)
         
-        if col - 1 >= 0 and grid[row][col - 1] == "1":
-            self.islandsHelper(grid, row, col - 1)
-        
-        if row - 1 >= 0 and grid[row - 1][col] == "1":
-            self.islandsHelper(grid, row - 1, col)
+        if col - 1 >= 0 and grid[row][col - 1] == '1':
+            self.helper(grid, row, col - 1)
