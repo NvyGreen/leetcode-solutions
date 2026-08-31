efb0c639f0dbb9cbaf6a1dc9d8ec999f506d282d
Nivedha Aravindmali <136926840+NvyGreen@users.noreply.github.com>
2026-08-31 00:56:05 +0000

    [LeetCode Sync] Runtime - 3 ms (94.99%), Memory - 19.5 MB (83.38%)

--- my-solutions/0039-combination-sum/solution.py
+++ my-solutions/0039-combination-sum/solution.py
@@ -1,25 +1,24 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         candidates.sort(reverse=True)
-        combos = []
-        for i in range(len(candidates)):
-            num = candidates[i]
-            combos += self.sumHelper(candidates, target, num, [num], i)
-        return combos
-    
-
-    def sumHelper(self, candidates: List[int], target: int, runningSum: int, runningList: List[int], index: int) -> List[List[int]]:
         result = []
-        if runningSum == target:
-            result.append(runningList)
-            return result
-        elif runningSum > target:
-            return result
-        
-        for i in range(index, len(candidates)):
-            num = candidates[i]
-            newList = runningList.copy()
-            newList.append(num)
-            result += self.sumHelper(candidates, target, runningSum + num, newList, i)
+
+        for i in range(len(candidates)):
+            if candidates[i] > target:
+                continue
+            result += self.helper(candidates, [candidates[i]], candidates[i], target, i)
         
         return result
+    
+
+    def helper(self, candidates: List[int], running: List[int], total: int, target: int, index: int) -> List[List[int]]:
+        if total == target:
+            return [running]
+        
+        solution = []
+        for i in range(index, len(candidates)):
+            if total + candidates[i] > target:
+                continue
+            solution += self.helper(candidates, running + [candidates[i]], total + candidates[i], target, i)
+        
+        return solution
