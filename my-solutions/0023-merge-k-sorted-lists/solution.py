b14448244a57bd75a1a5a0e49225dc0760bfdc14
Nivedha Aravindmali <136926840+NvyGreen@users.noreply.github.com>
2026-09-01 00:49:57 +0000

    [LeetCode Sync] Runtime - 15 ms (24.08%), Memory - 23.6 MB (5.75%)

--- my-solutions/0023-merge-k-sorted-lists/solution.py
+++ my-solutions/0023-merge-k-sorted-lists/solution.py
@@ -5,11 +5,11 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        if not lists:
+        if len(lists) == 0:
             return None
-        if len(lists) == 1:
+        elif len(lists) == 1:
             return lists[0]
-        
+
         mid = len(lists) // 2
         left = self.mergeKLists(lists[:mid])
         right = self.mergeKLists(lists[mid:])
@@ -17,20 +17,15 @@ class Solution:
         return self.merge(left, right)
     
 
-    def merge(self, l1: List[Optional[ListNode]], l2: List[Optional[ListNode]]) -> Optional[ListNode]:
-        dummy = ListNode()
-        curr = dummy
-
-        while l1 and l2:
-            if l1.val < l2.val:
-                curr.next = l1
-                l1 = l1.next
-            else:
-                curr.next = l2
-                l2 = l2.next
-            
-            curr = curr.next
+    def merge(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if not list1:
+            return list2
+        elif not list2:
+            return list1
         
-        curr.next = l1 or l2
-        return dummy.next
-        
+        if list1.val < list2.val:
+            list1.next = self.merge(list1.next, list2)
+            return list1
+        else:
+            list2.next = self.merge(list1, list2.next)
+            return list2
