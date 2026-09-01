90b7ba6527ef084975fdf535959a1213262562a8
Nivedha Aravindmali <136926840+NvyGreen@users.noreply.github.com>
2026-09-01 17:16:22 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.2 MB (95.63%)

--- my-solutions/0088-merge-sorted-array/solution.py
+++ my-solutions/0088-merge-sorted-array/solution.py
@@ -3,16 +3,18 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        e1 = m - 1
-        e2 = n - 1
-        p = len(nums1) - 1
+        p1, p2, i = m - 1, n - 1, len(nums1) - 1
 
-        while e2 >= 0:
-            if e1 >= 0 and nums1[e1] > nums2[e2]:
-                nums1[p] = nums1[e1]
-                e1 -= 1
+        while p1 >= 0 and p2 >= 0 and i >= 0:
+            if nums1[p1] >= nums2[p2]:
+                nums1[i] = nums1[p1]
+                p1 -= 1
             else:
-                nums1[p] = nums2[e2]
-                e2 -= 1
-            
-            p -= 1
+                nums1[i] = nums2[p2]
+                p2 -= 1
+            i -= 1
+        
+        while p2 >= 0 and i >= 0:
+            nums1[i] = nums2[p2]
+            p2 -= 1
+            i -= 1
