8199b9533887380d4feff3f6a6644c7112cc6509
Nivedha Aravindmali <136926840+NvyGreen@users.noreply.github.com>
2026-09-03 23:30:49 +0000

    [LeetCode Sync] Runtime - 7 ms (73.02%), Memory - 23.2 MB (16.46%)

--- my-solutions/0056-merge-intervals/solution.py
+++ my-solutions/0056-merge-intervals/solution.py
@@ -7,7 +7,7 @@ class Solution:
         for i in range(1, len(intervals)):
             currStart, currEnd = intervals[i]
             if currStart <= lastEnd:
-                result[-1] = [min(currStart, lastStart), max(currEnd, lastEnd)]
+                result[-1] = [min(lastStart, currStart), max(lastEnd, currEnd)]
             else:
                 result.append([currStart, currEnd])
             lastStart, lastEnd = result[-1]
