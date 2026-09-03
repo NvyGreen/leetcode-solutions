b0ad1356ef5b08cc7f8885722287a65cac58086a
Nivedha Aravindmali <136926840+NvyGreen@users.noreply.github.com>
2026-09-03 01:10:02 +0000

    [LeetCode Sync] Runtime - 11 ms (77.31%), Memory - 19.3 MB (75.96%)

--- my-solutions/0242-valid-anagram/solution.py
+++ my-solutions/0242-valid-anagram/solution.py
@@ -1,13 +1,13 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        freq = [0] * 26
+        freq = defaultdict(int)
         for c in s:
-            freq[ord(c) - ord('a')] += 1
+            freq[c] += 1
         
         for c in t:
-            freq[ord(c) - ord('a')] -= 1
-            if freq[ord(c) - ord('a')] < 0:
+            freq[c] -= 1
+            if freq[c] < 0:
                 return False
         
-        check = set(freq)
+        check = set(freq.values())
         return len(check) == 1 and 0 in check
