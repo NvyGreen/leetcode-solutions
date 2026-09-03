402a22fd83bb4485693a48442e7034d745aae03e
Nivedha Aravindmali <136926840+NvyGreen@users.noreply.github.com>
2026-09-03 01:05:45 +0000

    [LeetCode Sync] Runtime - 3740 ms (16.62%), Memory - 27.2 MB (5.38%)

--- my-solutions/0005-longest-palindromic-substring/solution.py
+++ my-solutions/0005-longest-palindromic-substring/solution.py
@@ -2,30 +2,23 @@ class Solution:
     def longestPalindrome(self, s: str) -> str:
         dp = []
         for _ in range(len(s)):
-            dp.append([False] * len(s))
+            row = [False] * len(s)
+            dp.append(row)
         
         for i in range(len(s)):
             dp[i][i] = True
         
-        bestLen = 1
-        bestIndex = 0
-
-        checkLen = 2
+        bestLen, bestIndex, checkLen = 1, 0, 2
         while checkLen <= len(s):
             for i in range(len(s)):
                 if checkLen == 2 and i + 1 < len(s) and s[i] == s[i + 1]:
                     dp[i][i + 1] = True
-                    if checkLen > bestLen:
-                        bestLen = checkLen
-                        bestIndex = i
+                    bestLen, bestIndex = checkLen, i
                 else:
                     j = i + checkLen - 1
                     if j < len(s) and s[i] == s[j] and dp[i + 1][j - 1]:
                         dp[i][j] = True
-                        if checkLen > bestLen:
-                            bestLen = checkLen
-                            bestIndex = i
-            
+                        bestLen, bestIndex = checkLen, i
             checkLen += 1
         
         return s[bestIndex:bestIndex + bestLen]
