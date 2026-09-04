f40ffaef277b5eb6f25045d39057db54182db848
Nivedha Aravindmali <136926840+NvyGreen@users.noreply.github.com>
2026-09-04 23:45:01 +0000

    [LeetCode Sync] Runtime - 1107 ms (65.23%), Memory - 19.5 MB (58.78%)

--- my-solutions/0300-longest-increasing-subsequence/solution.py
+++ my-solutions/0300-longest-increasing-subsequence/solution.py
@@ -3,7 +3,6 @@ class Solution:
         dp = [1] * len(nums)
         for i in range(1, len(nums)):
             for j in range(i):
-                if nums[j] < nums[i]:
+                if nums[i] > nums[j]:
                     dp[i] = max(dp[i], dp[j] + 1)
-        
         return max(dp)
