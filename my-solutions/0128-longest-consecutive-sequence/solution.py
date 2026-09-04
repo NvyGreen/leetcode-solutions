a2bc53422881cb04adfa1f3061c6815fb9d23d6c
Nivedha Aravindmali <136926840+NvyGreen@users.noreply.github.com>
2026-09-04 00:09:48 +0000

    [LeetCode Sync] Runtime - 47 ms (70.41%), Memory - 36.6 MB (66.64%)

--- my-solutions/0128-longest-consecutive-sequence/solution.py
+++ my-solutions/0128-longest-consecutive-sequence/solution.py
@@ -3,14 +3,11 @@ class Solution:
         if len(nums) == 0:
             return 0
         
-        longest = 1
-        lookup = set(nums)
-
+        longest, lookup = 1, set(nums)
         for num in lookup:
             if num - 1 not in lookup:
                 length = 0
                 while num + length in lookup:
                     length += 1
                 longest = max(longest, length)
-        
         return longest
