e3466d4905ccde5755e4141a119af07f6a0a213f
Nivedha Aravindmali <136926840+NvyGreen@users.noreply.github.com>
2026-09-04 00:27:23 +0000

    [LeetCode Sync] Runtime - 3 ms (31.75%), Memory - 19.3 MB (87.74%)

--- my-solutions/0046-permutations/solution.py
+++ my-solutions/0046-permutations/solution.py
@@ -1,21 +1,25 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        result = []
+        permutations = []
         visited = [False] * len(nums)
+
         for i in range(len(nums)):
             visited[i] = True
-            result += self.permuteHelper(nums, visited, [nums[i]])
+            permutations += self.helper(nums, visited, [nums[i]])
             visited[i] = False
-        return result
+        
+        return permutations
     
 
-    def permuteHelper(self, nums: List[int], visited: List[bool], running: List[int]) -> List[List[int]]:
+    def helper(self, nums: List[int], visited: List[bool], running: List[int]) -> List[List[int]]:
         if len(running) == len(nums):
             return [running]
+        
         result = []
         for i in range(len(nums)):
             if not visited[i]:
                 visited[i] = True
-                result += self.permuteHelper(nums, visited, running + [nums[i]])
+                result += self.helper(nums, visited, running + [nums[i]])
                 visited[i] = False
+        
         return result
