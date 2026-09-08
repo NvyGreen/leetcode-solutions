f0a6b23fc57b27d856d6c00dcb665caae866c00f
Nivedha Aravindmali <136926840+NvyGreen@users.noreply.github.com>
2026-09-08 20:58:37 +0000

    [LeetCode Sync] Runtime - 41 ms (85.44%), Memory - 19.2 MB (19.22%)

---
+++ my-solutions/1013-fibonacci-number/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    memo = {0: 0, 1: 1}
+    def fib(self, n: int) -> int:
+        if n in self.memo:
+            return self.memo[n]
+        self.memo[n] = self.fib(n - 1) + self.fib(n - 2)
+        return self.memo[n]
