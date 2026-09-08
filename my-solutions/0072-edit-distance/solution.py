fc01b3d0af5e5770dcadf2e58d4ea27633258e7f
Nivedha Aravindmali <136926840+NvyGreen@users.noreply.github.com>
2026-09-08 23:28:28 +0000

    [LeetCode Sync] Runtime - 63 ms (11.37%), Memory - 28 MB (5.21%)

--- my-solutions/0072-edit-distance/solution.py
+++ my-solutions/0072-edit-distance/solution.py
@@ -1,5 +1,10 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
+        if len(word1) == 0 or len(word2) == 0:
+            return max(len(word1), len(word2))
+        elif word1 == word2:
+            return 0
+        
         dp = []
         for i in range(len(word1) + 1):
             row = []
@@ -12,6 +17,6 @@ class Solution:
                 if word1[i - 1] == word2[j - 1]:
                     dp[i][j] = dp[i - 1][j - 1]
                 else:
-                    dp[i][j] = min(dp[i - 1][j - 1], dp[i][j - 1], dp[i - 1][j]) + 1
+                    dp[i][j] = min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]) + 1
         
-        return dp[len(word1)][len(word2)]
+        return dp[-1][-1]
