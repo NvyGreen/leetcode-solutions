3e84f1580e3caa051a4a401954e44a26d7a94f83
Nivedha Aravindmali <136926840+NvyGreen@users.noreply.github.com>
2026-09-13 18:22:45 +0000

    [LeetCode Sync] Runtime - 4068 ms (15.49%), Memory - 27.2 MB (6.59%)

--- my-solutions/0005-longest-palindromic-substring/solution.py
+++ my-solutions/0005-longest-palindromic-substring/solution.py
@@ -6,17 +6,19 @@ class Solution:
             row[i] = True
             dp.append(row)
         
-        bestLen, bestIndex, checkLen = 1, 0, 2
+
+        bestIndex, bestLen, checkLen = 0, 1, 2
         while checkLen <= len(s):
             for i in range(len(s)):
                 if checkLen == 2 and i + 1 < len(s) and s[i] == s[i + 1]:
                     dp[i][i + 1] = True
-                    bestLen, bestIndex = checkLen, i
+                    bestIndex, bestLen = i, checkLen
                 else:
                     j = i + checkLen - 1
                     if j < len(s) and s[i] == s[j] and dp[i + 1][j - 1]:
                         dp[i][j] = True
-                        bestLen, bestIndex = checkLen, i
+                        bestIndex, bestLen = i, checkLen
+            
             checkLen += 1
         
         return s[bestIndex:bestIndex + bestLen]
