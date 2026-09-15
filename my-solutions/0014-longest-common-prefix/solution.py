ec22ed03079ccd89e29b9d443aa87ac69bdd732a
Nivedha Aravindmali <136926840+NvyGreen@users.noreply.github.com>
2026-09-15 19:14:07 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.2 MB (73.46%)

--- my-solutions/0014-longest-common-prefix/solution.py
+++ my-solutions/0014-longest-common-prefix/solution.py
@@ -1,13 +1,10 @@
 class Solution:
-    def longestCommonPrefix(self, strs: List[str]) -> str:
+    def longestCommonPrefix(self, strs: list[str]) -> str:
         words = sorted(strs, key=len)
         prefix = words[0]
 
         for i in range(1, len(words)):
-            while len(prefix) > 0 and not words[i].startswith(prefix):
+            while not words[i].startswith(prefix):
                 prefix = prefix[:-1]
-
-            if len(prefix) == 0:
-                break
         
         return prefix
