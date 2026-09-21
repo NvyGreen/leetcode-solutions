bc75213edc4d03799617c81867f69277b3c8a46c
Nivedha Aravindmali <136926840+NvyGreen@users.noreply.github.com>
2026-09-21 22:08:25 +0000

    [LeetCode Sync] Runtime - 15 ms (97.83%), Memory - 21.4 MB (29.85%)

--- my-solutions/0744-network-delay-time/solution.py
+++ my-solutions/0744-network-delay-time/solution.py
@@ -1,22 +1,23 @@
 class Solution:
-    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+    def networkDelayTime(self, times: list[list[int]], n: int, k: int) -> int:
         graph = defaultdict(list)
-        for time in times:
-            graph[time[0]].append((time[1], time[2]))
+        for start, end, time in times:
+            graph[start].append((end, time))
         dists = [float('inf')] * n
         dists[k - 1] = 0
-        
+
         queue = [(0, k)]
         visited = set()
+
         while len(queue) > 0:
             dist, node = heapq.heappop(queue)
-            if node in visited or dists[node - 1] != dist or dist == float('inf'):
+            if node in visited or dists[node - 1] == float('inf') or dist != dists[node - 1]:
                 continue
             visited.add(node)
 
-            for neighbor, weight in graph[node]:
-                dists[neighbor - 1] = min(dists[neighbor - 1], dists[node - 1] + weight)
-                if dists[neighbor - 1] == dists[node - 1] + weight:
-                    heapq.heappush(queue, (dists[neighbor - 1], neighbor))
+            for end, time in graph[node]:
+                dists[end - 1] = min(dists[end - 1], dists[node - 1] + time)
+                if dists[end - 1] == dists[node - 1] + time:
+                    heapq.heappush(queue, (dists[end - 1], end))
         
         return max(dists) if len(visited) == n else -1
