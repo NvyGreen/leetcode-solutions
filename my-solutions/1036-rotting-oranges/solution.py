40b080d74c1842d15a716664599ba14d8a9b5072
Nivedha Aravindmali <136926840+NvyGreen@users.noreply.github.com>
2026-09-21 22:00:15 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.4 MB (43.29%)

--- my-solutions/1036-rotting-oranges/solution.py
+++ my-solutions/1036-rotting-oranges/solution.py
@@ -1,39 +1,44 @@
 class Solution:
-    def orangesRotting(self, grid: List[List[int]]) -> int:
+    def orangesRotting(self, grid: list[list[int]]) -> int:
         queue = deque([])
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
+        fresh = 0
+        m, n = len(grid), len(grid[0])
+
+        for i in range(m):
+            for j in range(n):
                 if grid[i][j] == 2:
-                    queue.append([i, j])
+                    queue.append((i, j))
+                elif grid[i][j] == 1:
+                    fresh += 1
         
         time = 0
         while len(queue) > 0:
             roundLength = len(queue)
+            
             for _ in range(roundLength):
                 row, col = queue.popleft()
 
-                if col + 1 < len(grid[0]) and grid[row][col + 1] == 1:
+                if col + 1 < n and grid[row][col + 1] == 1:
+                    fresh -= 1
                     grid[row][col + 1] = 2
-                    queue.append([row, col + 1])
+                    queue.append((row, col + 1))
                 
-                if row + 1 < len(grid) and grid[row + 1][col] == 1:
+                if row + 1 < m and grid[row + 1][col] == 1:
+                    fresh -= 1
                     grid[row + 1][col] = 2
-                    queue.append([row + 1, col])
+                    queue.append((row + 1, col))
                 
                 if col - 1 >= 0 and grid[row][col - 1] == 1:
+                    fresh -= 1
                     grid[row][col - 1] = 2
-                    queue.append([row, col - 1])
+                    queue.append((row, col - 1))
                 
                 if row - 1 >= 0 and grid[row - 1][col] == 1:
+                    fresh -= 1
                     grid[row - 1][col] = 2
-                    queue.append([row - 1, col])
+                    queue.append((row - 1, col))
             
             if len(queue) > 0:
                 time += 1
         
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == 1:
-                    return -1
-        
-        return time
+        return time if fresh <= 0 else -1
