d21d75be3f6a9c9e5493f99a2a7fd035ddefe050
Nivedha Aravindmali <136926840+NvyGreen@users.noreply.github.com>
2026-09-21 21:06:32 +0000

    [LeetCode Sync] Runtime - 35 ms (15.69%), Memory - 23.5 MB (12.17%)

--- my-solutions/0064-minimum-path-sum/solution.py
+++ my-solutions/0064-minimum-path-sum/solution.py
@@ -1,32 +1,30 @@
 class Solution:
-    def minPathSum(self, grid: List[List[int]]) -> int:
+    def minPathSum(self, grid: list[list[int]]) -> int:
         m, n = len(grid), len(grid[0])
         dp = []
-        for i in range(m):
-            row = [-1] * n
-            dp.append(row)
+        for _ in range(m):
+            dp.append([-1] * n)
         
-        self.pathHelper(grid, dp, m, n, 0, 0)
-        return dp[0][0]
-    
 
-    def pathHelper(self, grid: List[List[int]], dp: List[List[int]], m: int, n: int, row: int, col: int) -> None:
-        if dp[row][col] != -1:
-            return
+        def search(row: int, col: int):
+            if row == m - 1 and col == n - 1:
+                dp[row][col] = grid[row][col]
+                return
+            
+            rightSum = float('inf')
+            if col + 1 < n:
+                if dp[row][col + 1] == -1:
+                    search(row, col + 1)
+                rightSum = dp[row][col + 1]
+            
+            downSum = float('inf')
+            if row + 1 < m:
+                if dp[row + 1][col] == -1:
+                    search(row + 1, col)
+                downSum = dp[row + 1][col]
+            
+            dp[row][col] = min(rightSum, downSum) + grid[row][col]
         
-        if row == m - 1 and col == n - 1:
-            dp[row][col] = grid[row][col]
-            return
-        
-        rightSum = float('inf')
-        if col + 1 < n:
-            self.pathHelper(grid, dp, m, n, row, col + 1)
-            rightSum = dp[row][col + 1]
-        
-        downSum = float('inf')
-        if row + 1 < m:
-            self.pathHelper(grid, dp, m, n, row + 1, col)
-            downSum = dp[row + 1][col]
-        
-        dp[row][col] = grid[row][col] + min(rightSum, downSum)
-        
+
+        search(0, 0)
+        return dp[0][0]
