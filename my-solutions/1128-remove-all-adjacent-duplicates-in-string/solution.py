ea97f640d52f7bed1c960ac871c45e0d7267bf82
Nivedha Aravindmali <136926840+NvyGreen@users.noreply.github.com>
2026-09-23 19:52:52 +0000

    [LeetCode Sync] Runtime - 27 ms (20.33%), Memory - 20.2 MB (29.21%)

---
+++ my-solutions/1128-remove-all-adjacent-duplicates-in-string/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def removeDuplicates(self, s: str) -> str:
+        stk = []
+        for c in s:
+            if len(stk) > 0 and stk[-1] == c:
+                stk.pop()
+            else:
+                stk.append(c)
+        return ''.join(stk)
